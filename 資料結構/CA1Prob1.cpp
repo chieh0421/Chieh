b620e788a49de6a227e4eefccf56475c29ffce77
chieh0421 <32053232+chieh0421@users.noreply.github.com>
2018-10-03 15:08:24 +0800

    Update

--- 資料結構/CA1Prob1.cpp
+++ 資料結構/CA1Prob1.cpp
@@ -1,20 +1,31 @@
+/*
+ Briefly explain the purpose and functionality of this file (program).
+ @author Ming Chieh Chen
+ @ID     B06505047
+ @Department Engineering Science and Ocean Engineering
+ @Affiliation National Taiwan University
+*/
+
 #include <iostream>
 using namespace std;
 int dotProduct(int a[],int b[],int n)
 {
 	int result = 0;
+	for (int i = 0; i < n; i++)
+	{
+		result += a[i] * b[i];//矩陣元素相乘，並加入結果中
+	}
 	return result;
 }
 
 int main() {
 	int a[] = { -1,0,2,15,7,6,-4,8,21,-13 }; 
 	int b[] = { 5,9,-18,16,0,1,-4,18,-2,12 };
-	int c[5];
 	int n;
 	if (sizeof(a) >= sizeof(b))
-		n = sizeof(a)/sizeof(int);
-	else
 		n = sizeof(b)/sizeof(int);
+	else
+		n = sizeof(a)/sizeof(int);
 	cout << dotProduct(a, b, n) << endl;
 	system("PAUSE");
 	return 0;
