77d6469bc67a3678c11995e63bbf34954584cbd5
chieh0421 <32053232+chieh0421@users.noreply.github.com>
2018-10-22 21:04:45 +0800

    Upload

---
+++ GDRC/MazeBall/Assets/Bean.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bean : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name == "Player")
+        {
+            Destroy(gameObject);
+            collider.GetComponent<Player>().addScore();
+        }
+    }
+
+}
+
---
+++ GDRC/MazeBall/Assets/MagicBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicBox : MonoBehaviour {
+    public Bean beanPrefab;
+    bool isUsed;
+    void Start()
+    {
+        isUsed = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if(!isUsed && other.name == "Player")
+        {
+            isUsed = true;
+            Debug.Log("Collison");
+            Bean bean = Instantiate(beanPrefab);
+            Vector3 diff = transform.position - other.transform.position;
+            Vector3 pos = transform.position + diff;
+            bean.transform.position = pos;
+        }
+    }
+}
