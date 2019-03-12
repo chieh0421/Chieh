a3702ad065c097573ae81a7934fad84806254dc8
chieh0421 <32053232+chieh0421@users.noreply.github.com>
2019-03-12 17:17:36 +0800

    Update HW1.cpp

--- 大二下/Data Structures and Algorithms/HW1.cpp
+++ 大二下/Data Structures and Algorithms/HW1.cpp
@@ -2,39 +2,43 @@
 #include <map>
 #include <algorithm>
 using namespace std;
+typedef pair<int, int>pp;
 
+bool compare(const pp&left, const pp& right) {
+	return left.second < right.first;
+}
+
+map<int, int>::iterator find(map<int, int>::iterator start, map<int, int>::iterator end, int value) {
+	while (start != end) {
+		if (start->first == value)
+			return start;
+		start++;
+	}
+	return end;
+}
 
 int main()
 {
 	int N, M, K, cur;
 	cin >> N >> M >> K;
-	/*deque<int>  window;
-	int *int_arr = new int[N];
+	map<int,int> int_map;
+	map<int, int>::iterator it;
 	for (int i = 0; i < N; i++) {
 		cin >> cur;
-		int_arr[i] = cur;
-		if (i < M - 1)
-			window.push_back(cur);
-		else if (i == M - 1) {
-			window.push_back(cur);
-			sort(window.begin(), window.end());
-			cout << window[K - 1] << endl;
-		}
-		else {
-			window.erase(lower_bound(window.begin(), window.end(), int_arr[i - M]));
-			window.insert(lower_bound(window.begin(), window.end(), cur), cur);
-			cout << window[K - 1] << endl;
-		}
-	}
-	delete[] int_arr;
-	return 0;*/
-	map<int, int> int_map;
-	for (int i = 0; i < N; i++) {
-		cin >> cur;
-		int_map.insert(i,cur);
+		int_map.insert(pp(i + 1, cur));
 		if (i == M - 1) {
-			sort(int_map.begin(), int_map.end());
-			cout <<  << endl;
+			sort(int_map.begin(), int_map.end(),compare);
+			for (int j = 1; j < K; j++)
+				it++;
+			cout << it->second << endl;
+		}
+		else if (i >= M) {
+			int_map.erase(find(int_map.begin(), int_map.end(), i - M + 1));
+			cout << it->second << endl;
 		}
 	}
 }
+
+
+
+
