79864d71df6553322e5b78b9c07bbe30e6759686
chieh0421 <32053232+chieh0421@users.noreply.github.com>
2019-04-30 16:16:18 +0800

    Update HW3.cpp

--- 大二下/Data Structures and Algorithms/HW3.cpp
+++ 大二下/Data Structures and Algorithms/HW3.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#define MAXIMUMPOINT = 193;
-#define MINIMUMPOINT = -193;
+#define MAXIMUMPOINT 193;
+#define MINIMUMPOINT -193;
 #define ull unsigned long long
 
+//global variable
+int N;
+int *AliceCards, *BobCards;
+
+long boolArrToLong(bool* arr) {
+	long result = 0;
+	for (int i = 0; i < sizeof(arr) / sizeof(bool); i++) {
+		result += arr[i] << (N - i);
+	}
+	return result;
+}
+
+bool* boolArrCopy(bool* arr) {
+	int length = sizeof(arr) / sizeof(bool);
+	bool* result = new bool[length];
+	for (int i = 0; i < length; i++) {
+		result[i] = arr[i];
+	}
+	return result;
+}
+
+int point(bool*arr, int*cards) {
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		if (arr[i])
+			result += cards[i] % 13 == 0 ? 13 : cards[i] % 13;
+	}
+	return result;
+}
 
 int cardToIndex(string card) {
-	int suit;
-	int number;
+	int suit = -1;
+	int number = -1;
 	int index = 0;
 	switch (card.at(0)) {
 	case 'C':
@@ -25,8 +54,8 @@ int cardToIndex(string card) {
 		break;
 	}
 	index += suit * 13;
-	if (card.at(1) >= 2 && card.at(1) <= 9) {
-		number = card.at(1) - 1;
+	if (card.at(1) >= '2' && card.at(1) <= '9') {
+		number = card.at(1) - '1';
 	}
 	else {
 		switch (card.at(1)) {
@@ -51,65 +80,88 @@ int cardToIndex(string card) {
 	return index;
 }
 
-int alphabeta(ull state, int alpha, int beta) {
-	//state: (Alice's card)*N + (Bob's card)*N + (Alice:1 Bob:0)*1 + (previous card)*6 + (alpha)*6 + (beta)*6
-	
+int alphabeta(bool* alice, bool* bob, bool player, int previousCard, int alpha, int beta) {
+	if ((boolArrToLong(alice) | 0) == 0) {
+		return point(bob, BobCards);
+	}
+	else if ((boolArrToLong(bob) | 0) == 0) {
+		return point(alice, AliceCards);
+	}
+	int value;
+	if (player) { //player: Alice -> max
+		value = MINIMUMPOINT;
+		for (int i = 0; i < N; i++) {
+			bool haveCardToUse = false;
+			if (alice[i] && ((AliceCards[i] % 13 == previousCard % 13 || AliceCards[i] / 13 == previousCard / 13) || previousCard == -1)) {
+				if (!haveCardToUse)
+					haveCardToUse = true;
+				bool* parameterAlice = boolArrCopy(alice);
+				parameterAlice[i] = false;
+				value = alphabeta(parameterAlice, bob, !player, AliceCards[i], value, beta);
+				if (alpha > value ? alpha : value >= beta)
+					break;
+			}
+			if (haveCardToUse)
+				return value;
+			else
+				return alphabeta(alice, bob, !player, -1, alpha, beta);
+		}
+		return value;
+	}
+	else { //player: Bob -> min
+		value = MAXIMUMPOINT;
+		for (int i = 0; i < N; i++) {
+			bool haveCardToUse = false;
+			if (bob[i] && ((BobCards[i] % 13 == previousCard % 13 || BobCards[i] / 13 == previousCard / 13) || previousCard == -1)) {
+				if (!haveCardToUse)
+					haveCardToUse = true;
+				bool* parameterBob = boolArrCopy(bob);
+				parameterBob[i] = false;
+				value = alphabeta(alice, parameterBob, !player, BobCards[i], alpha, value);
+				if (beta > value ? value : beta >= alpha)
+					break;
+			}
+			if (haveCardToUse)
+				return value;
+			else
+				return alphabeta(alice, bob, !player, -1, alpha, beta);
+		}
+		return value;
+	}
 }
 
-class Hand {
-	int *cards;
-	int noOfSuit[4];
-	int noOfNumber[13];
-	int noOfcards;
-public:
-	Hand(int N) {
-		for (int& i : noOfSuit)
-			i = 0;
-		for (int& i : noOfNumber)
-			i = 0;
-		cards = new int(N);
-		for (int i = 0; i < N; i++)
-			cards[i] = -1;
-		noOfcards = 0;
-	}
-	void get(int card) {
-		cards[noOfcards] = card;
-		noOfcards++;
-		card % 13 == 0 ? noOfNumber[12]++ : noOfNumber[card % 13 - 1]++;
-		card / 13 == 0 ? noOfSuit[card / 13 - 1]++ : noOfSuit[card / 13]++;
-	}
-	void use(int card) {
-		for (int i = 0; i < sizeof(cards) / sizeof(int); i++) {
-			if (cards[i] == card) {
-				cards[i] = -1;
-				noOfcards--;
-				break;
-			}				
-		}
-		card % 13 == 0 ? noOfNumber[12]-- : noOfNumber[card % 13 - 1]--;
-		card / 13 == 0 ? noOfSuit[card / 13 - 1]-- : noOfSuit[card / 13]--;
-	}
-};
-
-//global variable
-int N;
-int *AliceCards, *BobCards;
+ull stateGenerateFunction(bool* alice, bool* bob, bool player, int previousCard, int alpha, int beta) {
+	//state: (Alice's card)*N + (Bob's card)*N + (Alice:1 Bob:0)*1 + (previous card)*6 + (alpha)*9 + (beta)*9
+	ull state = 0;
+	state += boolArrToLong(alice) << (25 + N);
+	state += boolArrToLong(bob) << 25;
+	if (player)
+		state += 1 << 24;
+	state += previousCard << 18;
+	state += alpha << 9;
+	state += beta;
+	return state;
+}
 
 int main()
 {
 	cin.tie(0);
 	cin >> N;
 	AliceCards = new int[N];
+	Alice = new bool[N];
 	BobCards = new int[N];
+	Bob = new bool[N];
 	string card;
 	for (int i = 0; i < N; i++) { //Alice's cards
 		cin >> card;
 		AliceCards[i] = cardToIndex(card);
+		Alice[i] = true;
 	}
 	for (int i = 0; i < N; i++) { //Bob's cards
 		cin >> card;
 		BobCards[i] = cardToIndex(card);
+		Bob[i] = true;
 	}
-	
+	cout << alphabeta(Alice, Bob, true, -1, 193, 193);
 	return 0;
 }
