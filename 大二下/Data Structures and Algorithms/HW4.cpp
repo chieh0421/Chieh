6092fceeb4df568ae4f313e0fdf0f8d98e2ade91
chieh0421 <32053232+chieh0421@users.noreply.github.com>
2019-05-21 14:50:55 +0800

    Update HW4.cpp

--- 大二下/Data Structures and Algorithms/HW4.cpp
+++ 大二下/Data Structures and Algorithms/HW4.cpp
@@ -3,10 +3,11 @@
 #include <vector>
 #include <map>
 #include <math.h>
+#include <algorithm>
 #define ul unsigned long
 using namespace std;
 
-vector<ul> hash1, hash2, hash1ByLength, hash2ByLength;
+vector<ul> hash1, hash2, hash1ByLength;
 vector<int> v;
 string s;
 int times;
@@ -20,30 +21,32 @@ void hashInit() {
 			hash2.push_back(s[i] - 'a');
 		}
 		else {
-			hash1.push_back((hash1[i - 1] * 26 + (s[i] - 'a')) % (UINT32_MAX));
-			hash2.push_back((hash2[i - 1] * 27 + (s[i] - 'a')) % (UINT32_MAX));
+			hash1.push_back((hash1[i - 1] << 5 + (s[i] - 'a')) % (UINT32_MAX));
+			hash2.push_back((hash2[i - 1] << 6 + (s[i] - 'a')) % (UINT32_MAX));
 		}
 	}
 }
 
 void hashByLengthInit(int length) {
 	hash1ByLength.clear();
-	hash2ByLength.clear();
 	hash1ByLength.push_back(hash1[length - 1]);
-	hash2ByLength.push_back(hash2[length - 1]);
 	for (int i = length; i < int(s.length()); i++) {
-		hash1ByLength.push_back(hash1[i] - ((hash1[i - length] * int(pow(26, length)))));
-		hash2ByLength.push_back(hash2[i] - ((hash2[i - length] * int(pow(27, length)))));
+		hash1ByLength.push_back(hash1[i] - ((hash1[i - length] << 5 * length)));
 	}
 }
 
-int p1Check() {
+inline int subStringHash2(int startPos, int length) {
+	return hash2[startPos + length - 1] - startPos == 0 ? 0 : (hash2[startPos - 1] << 6 * length);
+}
+
+int p1Check(int length) {
 	int counter, currentMaxTimes = 0, rightestPos = -1;
 	for (int i = 0; i < int(hash1ByLength.size()); i++) {
 		counter = 1;
 		for (int j = 0; j < i; j++) {
-			if (hash1ByLength[j] == hash1ByLength[i] && hash2ByLength[j] == hash2ByLength[i]) {
-				counter++;
+			if (hash1ByLength[j] == hash1ByLength[i]) {
+				if (subStringHash2(i, length) == subStringHash2(j, length))
+					counter++;
 			}
 		}
 		if (counter >= currentMaxTimes) {
@@ -54,31 +57,15 @@ int p1Check() {
 	return currentMaxTimes >= times ? rightestPos : -1;
 }
 
-int p1Check2() {
-	int counter;
-	for (int i = 0; i < int(hash1ByLength.size()); i++) {
-		counter = 1;
-		for (int j = 0; j < i; j++) {
-			if (hash1ByLength[j] == hash1ByLength[i] && hash2ByLength[j] == hash2ByLength[i]) {
-				counter++;
-			}
-		}
-		if (counter >= times) {
-			return 1;
-		}
-	}
-	return -1;
-}
-
 pair<int, int> p1Search(int minLength, int maxLength) {
+	cout << "range:" << minLength << " " << maxLength << "\n";
 	if (maxLength == minLength || maxLength - minLength == 1) {
-		hashByLengthInit(minLength);
-		int pos = p1Check();
+		int pos = p1Check(minLength);
 		return pos == -1 ? pair<int, int>(-1, 0) : pair<int, int>(minLength, pos);
 	}
 	int midLength = (minLength + maxLength) / 2;
 	hashByLengthInit(midLength);
-	if (p1Check2() == -1)
+	if (p1Check(midLength) == -1)
 		return p1Search(minLength, midLength);
 	else
 		return p1Search(midLength, maxLength);
@@ -97,21 +84,20 @@ void problemOne() {
 				return;
 			}
 		}
-		cout << s.length() <<" "<< 0 << "\n";
+		cout << s.length() << " " << 0 << "\n";
 		return;
 	}
 	else {
 		hashInit();
-		hashByLengthInit(s.length());
-		if (p1Check2() == -1) {
+		if (count(s.begin(), s.end(), s[0]) == s.length()) {
+			cout << s.length() << " " << 0 << "\n";			
+		}
+		else {
 			pair<int, int>answer = p1Search(1, s.length());
 			if (answer.first == -1)
 				cout << "none\n";
 			else
 				cout << answer.first << " " << answer.second << "\n";
-		}
-		else {
-			cout << s.length() << " " << 0 << "\n";
 		}
 	}
 }
